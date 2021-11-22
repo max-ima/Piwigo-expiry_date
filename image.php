613774a5390eb49e409a2798dd3b6c5ae177556a
HWFord <lilypickle@hotmail.fr>
2021-11-22 10:03:48 +0100

    On image page in gallery adds expired on date
    if expired on date is sets display

--- image.php
+++ image.php
@@ -10,29 +10,44 @@ function expd_loc_end_picture()
 {
 	global $template, $picture;
 
+  $template->set_prefilter('picture', 'expd_picture_prefilter');
+
   list($dbnow) = pwg_db_fetch_row(pwg_query('SELECT NOW();'));
   
-  if (!isset($picture['current']['expiry_date'])){
+  if (!isset($picture['current']['expiry_date']) and !isset($picture['current']['expd_expired_on']))
+  {
     return;
   }
 
-  $expiry_date = $picture['current']['expiry_date'];
+  if (isset($picture['current']['expiry_date']))
+  {
+    $expiry_date = $picture['current']['expiry_date'];
 
-  $datetime1 = new DateTime($expiry_date);
-  $datetime2 = new DateTime($dbnow);
-  $interval = $datetime2->diff($datetime1);
-  $interval = $interval->format("%r%a");
+    $datetime1 = new DateTime($expiry_date);
+    $datetime2 = new DateTime($dbnow);
+    $interval = $datetime2->diff($datetime1);
+    $interval = $interval->format("%r%a");
 
-  $expiry_date = strftime('%A %d  %B %G', strtotime($picture['current']['expiry_date']));
-  
-  $template->assign(
-    array	(
-      'expiry_date' => $expiry_date,
-      'days' => $interval,
-    )
-  );
+    $expiry_date = strftime('%A %d  %B %G', strtotime($picture['current']['expiry_date']));
+    $template->assign(
+      array	(
+        'expiry_date' => $expiry_date,
+        'expd_days' => $interval,
+      )
+    );
+  }
 
-	$template->set_prefilter('picture', 'expd_picture_prefilter');
+  if (isset($picture['current']['expd_expired_on']))
+  {
+    $expired_on_date = strftime('%A %d  %B %G', strtotime($picture['current']['expd_expired_on']));
+    $template->assign(
+      array	(
+        'expired_on_date' => $expired_on_date,
+      )
+    );
+  }
+
+
 }
 
 /**
@@ -40,20 +55,31 @@ function expd_loc_end_picture()
  */
 function expd_picture_prefilter($content)
 {
-  global $template, $page, $data;
   //TODO make compatible with modus, bootstrap and elegant
 	// Search for image info block
-	$search = '#id="info-content" class="d-flex flex-column">#';
+	$search = '{if $display_info.rating_score';
 	
-  //add expiry date to image info block
-	$replacement = 'id="info-content" class="d-flex flex-column">
+  //add expiry date to image info block or expire on date
+
+  $replace = '
+{if isset($expiry_date)}
 <div id="expd_expiry_date"" class="imageInfo">
   <dl class="row mb-0">
     <dt class="col-sm-5">{\'Expiry date\'|@translate}</dt>
-    <dd class="col-sm-7">{$expiry_date}, in {$days} days</dd>
+    <dd class="col-sm-7">{$expiry_date}, in {$expd_days} days</dd>
   </dl>
 </div>
-';
+{/if}
 
-	return preg_replace($search, $replacement, $content);
+{if isset($expired_on_date)}
+<div id="expd_expiry_date"" class="imageInfo">
+  <dl class="row mb-0">
+    <dt class="col-sm-5">{\'expired on\'|@translate}</dt>
+    <dd class="col-sm-7">{$expired_on_date}</dd>
+  </dl>
+</div>
+{/if}
+    '.$search;
+
+	return str_replace($search, $replace, $content);
 }
